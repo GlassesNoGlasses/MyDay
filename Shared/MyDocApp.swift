cc116470485561eb18d2fcceea9034b0e593c7ec
Neil Wang <neilywang3@gmail.com>
2023-02-13 10:19:01 -0500

    Initial Commit

---
+++ Shared/MyDocApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyDocApp.swift
+//  Shared
+//
+//  Created by Neil Wang on 2023-02-13.
+//
+
+import SwiftUI
+
+@main
+struct MyDocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
